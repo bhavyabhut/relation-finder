b4a92503563e63d0dbe6da318d69b063b5cd8f23
Bhavya Bhut <pbhavy4@gmail.com>
2020-10-04 12:24:05 +0530

    change name

--- client/src/redux/requesting/requestingReducer.js
+++ client/src/redux/requesting/requestingReducer.js
@@ -1,17 +1,18 @@
-import MainReducer from '../../utilities/mainReducer'
-import RequestingActions from './actions'
-import { getKeyForAction } from '../../utilities/actionUtility'
+import MainReducer from "../../utilities/mainReducer";
+import RequestingActions from "./actions";
+import { getKeyForAction } from "../../utilities/actionUtility";
 
-const initialState = {}
+const initialState = {};
 export default MainReducer(
   initialState,
   {
     [RequestingActions.SET_REQUESTING](state, action) {
       return {
         ...state,
-        [getKeyForAction(action.payload.type, action.meta?.scope)]: action.payload.value,
-      }
+        [getKeyForAction(action.payload.type, action.meta?.scope)]: action
+          .payload.value,
+      };
     },
   },
-  false,
-)
+  false
+);
